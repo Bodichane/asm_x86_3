576e872bf071562ee7048ddf249c8fb02d812893
HP <HP@DESKTOP-7AETL76>
2024-12-15 21:24:45 +0300

    Добавьте файлы проекта.

---
+++ lab3/lab3.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <random>
+
+int main()
+{
+	std::random_device dev;
+	std::mt19937 generator(dev());
+	std::uniform_int_distribution<unsigned short> distribution(0, 65535);
+
+	const size_t size = 512;
+	unsigned short mass[size];
+	unsigned short data[size] = {0};
+
+	for (size_t i = 0; i < size; i++)
+	{
+		mass[i] = distribution(generator);
+	}
+
+	__asm {
+		pushad
+		lea edi, mass
+		lea esi, data
+		mov ebx, 0
+
+		dist:
+			mov ax, word ptr[edi]
+			cmp ax, 50000
+			jae big_number
+			cmp ax, 10000
+			jb less_number
+			test ax, 1
+			jz even_number
+			jmp odd_number
+
+		big_number:
+			cmp ebx, 128
+			je end_loop
+			mov [esi + ebx*2 + 512], ax
+			inc ebx
+			jmp increment_number
+
+		less_number:
+			cmp ebx, 128
+			je end_loop
+			mov [esi + ebx*2 + 768], ax
+			inc ebx
+			jmp increment_number
+
+		even_number:
+			cmp ebx, 128
+			je end_loop
+			mov [esi + ebx*2], ax
+			inc ebx
+			jmp increment_number
+
+		odd_number:
+			cmp ebx, 128
+			je end_loop
+			mov [esi + ebx*2 +256], ax
+			inc ebx
+			jmp increment_number
+
+		increment_number:
+			add edi, 2
+			jmp dist
+
+		end_loop:
+			popad
+	}
+	for (int i = 0; i < size; i++) {
+		std::cout << std::setw(8) << mass[i] << "   ----->   " << std::setw(5) << data[i] << "   i = " << i << std::endl;
+	}   
+    return 0;
+}
